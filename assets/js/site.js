db72860c4dd4e5d973f2c28b140274b0138854f3
Daniel Spano <dansonline@gmail.com>
2022-01-24 11:18:22 +1100

    Updated content

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,93 +1,93 @@
 $(document).ready(function () {
 
-    function isIOS() {
-        return /iP(ad|hone|od).+Version\/[\d\.]+.*Safari/i.test(navigator.userAgent);
-    }
+    // function isIOS() {
+    //     return /iP(ad|hone|od).+Version\/[\d\.]+.*Safari/i.test(navigator.userAgent);
+    // }
 
-    var isMenuShowing = false;
-    var isToggled = false;
-    var isToggleFinished = false;
-    var menuHeight = 60;
+    // var isMenuShowing = false;
+    // var isToggled = false;
+    // var isToggleFinished = false;
+    // var menuHeight = 60;
 
-    var scrollPivot = null;
-    var scrollDistanceUp = 0;
-    var scrollDistanceDown = 0;
+    // var scrollPivot = null;
+    // var scrollDistanceUp = 0;
+    // var scrollDistanceDown = 0;
 
-    var mainScrollPosition;
+    // var mainScrollPosition;
 
-    var toggleBooking = function (isVoucher) {
-        //$('.main-content').toggleClass('toggled');
-        var height = window.innerHeight;
-        var toggleOffset = $('.main-content').scrollTop() > 300 ? (height - menuHeight) : height;
-        if (window.innerWidth < 568) toggleOffset += 60;
+    // var toggleBooking = function (isVoucher) {
+    //     //$('.main-content').toggleClass('toggled');
+    //     var height = window.innerHeight;
+    //     var toggleOffset = $('.main-content').scrollTop() > 300 ? (height - menuHeight) : height;
+    //     if (window.innerWidth < 568) toggleOffset += 60;
 
-        if (isIOS())
-            $(window).scrollTop(mainScrollPosition);
+    //     if (isIOS())
+    //         $(window).scrollTop(mainScrollPosition);
 
 
-        if (isToggled) {
-            $('.booking-content').removeClass('toggled').velocity({ translateY: 0 }, 500, "swing", function () {
-                //$('.booking-content').fadeOut();
-                //$('.booking-content').animate({ opacity: 0 });
-                $('#booking_frame').hide();
-                $('#voucher_frame').hide();
-                isToggleFinished = false;
-            });
-            $('.menu-bar').removeClass('toggled');
-            $('html, body').css({ overflow: 'scroll' });
-            $('.menu-bar .book-button').html('Book a table');
-            $('.menu-bar .voucher-button').show();
-            $('.menu-bar .menu-logo').show();
-        } else {
-            if (isIOS()) {
-                mainScrollPosition = $(window).scrollTop();
-                $(window).scrollTop(0);
-            }
-            $('.booking-content').show();
-            isVoucher ? $('#voucher_frame').show() : $('#booking_frame').show();
-            isVoucher ? $('.booking-text').hide() : $('.booking-text').show();
-            $('.booking-content').addClass('toggled').velocity({ translateY: toggleOffset }, 500, "swing");
-            $('.menu-bar').addClass('toggled');
-            isVoucher ? $('.menu-bar .book-button').html('Close voucher') : $('.menu-bar .book-button').html('Close booking');
-            $('.menu-bar .voucher-button').hide();
-            $('.menu-bar .menu-logo').hide();
-            $('.booking-content').scrollTop(0);
-            $('html, body').css({ overflow: 'hidden' });
+    //     if (isToggled) {
+    //         $('.booking-content').removeClass('toggled').velocity({ translateY: 0 }, 500, "swing", function () {
+    //             //$('.booking-content').fadeOut();
+    //             //$('.booking-content').animate({ opacity: 0 });
+    //             $('#booking_frame').hide();
+    //             $('#voucher_frame').hide();
+    //             isToggleFinished = false;
+    //         });
+    //         $('.menu-bar').removeClass('toggled');
+    //         $('html, body').css({ overflow: 'scroll' });
+    //         $('.menu-bar .book-button').html('Book a table');
+    //         $('.menu-bar .voucher-button').show();
+    //         $('.menu-bar .menu-logo').show();
+    //     } else {
+    //         if (isIOS()) {
+    //             mainScrollPosition = $(window).scrollTop();
+    //             $(window).scrollTop(0);
+    //         }
+    //         $('.booking-content').show();
+    //         isVoucher ? $('#voucher_frame').show() : $('#booking_frame').show();
+    //         isVoucher ? $('.booking-text').hide() : $('.booking-text').show();
+    //         $('.booking-content').addClass('toggled').velocity({ translateY: toggleOffset }, 500, "swing");
+    //         $('.menu-bar').addClass('toggled');
+    //         isVoucher ? $('.menu-bar .book-button').html('Close voucher') : $('.menu-bar .book-button').html('Close booking');
+    //         $('.menu-bar .voucher-button').hide();
+    //         $('.menu-bar .menu-logo').hide();
+    //         $('.booking-content').scrollTop(0);
+    //         $('html, body').css({ overflow: 'hidden' });
 
-        }
+    //     }
 
-        isToggled = !isToggled;
-        if (isToggled) isToggleFinished = true;
+    //     isToggled = !isToggled;
+    //     if (isToggled) isToggleFinished = true;
 
-    }
+    // }
 
-    $('.book-button').click(function () {
-        toggleBooking(false);
-    });
+    // $('.book-button').click(function () {
+    //     //toggleBooking(false);
+    // });
 
-    $('.voucher-button').click(function () {
-        toggleBooking(true);
-    });
+    // $('.voucher-button').click(function () {
+    //     toggleBooking(true);
+    // });
 
-    var showMenu = function () {
+    // var showMenu = function () {
 
-        if (!isMenuShowing) {
-            $('.menu-bar').animate({ transform: 'translateY(0)' })
+    //     if (!isMenuShowing) {
+    //         $('.menu-bar').animate({ transform: 'translateY(0)' })
 
-            isMenuShowing = true;
-        }
+    //         isMenuShowing = true;
+    //     }
 
-    };
+    // };
 
-    var hideMenu = function () {
+    // var hideMenu = function () {
 
-        if (isMenuShowing) {
-            $('.menu-bar').animate({ marginTop: 'translateY(' + -menuHeight + 'px)' })
+    //     if (isMenuShowing) {
+    //         $('.menu-bar').animate({ marginTop: 'translateY(' + -menuHeight + 'px)' })
 
-            isMenuShowing = false;
-        }
+    //         isMenuShowing = false;
+    //     }
 
-    }
+    // }
 
     var percentageToOpacity = function (percentage, offset) {
         var distance = (percentage * 100);
